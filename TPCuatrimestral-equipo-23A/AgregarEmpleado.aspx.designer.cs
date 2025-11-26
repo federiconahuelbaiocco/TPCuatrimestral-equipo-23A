7abd3f1cdd7c79b2da4ef591cfe5b433fd534e82
Ignacio <nachofargas23@gmail.com>
2025-11-26 03:17:45 -0300

    Validaciones completas en Pacientes y Empleados (Front y Back)

--- TPCuatrimestral-equipo-23A/AgregarEmpleado.aspx.designer.cs
+++ TPCuatrimestral-equipo-23A/AgregarEmpleado.aspx.designer.cs
@@ -132,6 +132,33 @@ namespace TPCuatrimestral_equipo_23A
         protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvSexo;
 
         /// <summary>
+        /// Control txtFechaNac.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtFechaNac;
+
+        /// <summary>
+        /// Control rfvFechaNac.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvFechaNac;
+
+        /// <summary>
+        /// Control rvFechaNac.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RangeValidator rvFechaNac;
+
+        /// <summary>
         /// Control txtTelefono.
         /// </summary>
         /// <remarks>
